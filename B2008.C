5dd03195dd59bce93c2a90aefe0b961af1fec1c4
DGG <76085770+d25241gg1297@users.noreply.github.com>
2022-12-05 09:08:37 +0800

    Add files via upload

---
+++ B2008.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int g;
+    g = (a + b) * c;
+    printf("%d", g);
+
+
+    return 0;
+}
---
+++ B2009.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    int e;
+    e = a + b;
+
+    printf("%d", e / c);
+
+
+
+    return 0;
+}
