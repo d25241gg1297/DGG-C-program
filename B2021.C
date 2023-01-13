b8b94f844c4d939fa962b739280a34ca15d81863
DGG <76085770+d25241gg1297@users.noreply.github.com>
2023-01-13 20:07:12 +0800

    Add files via upload

---
+++ B2021.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float number;
+    scanf("%f\n", &number);
+
+    printf("%.3f", number);
+
+    return 0;
+}
---
+++ B2022.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    double Nb;
+    scanf("%lf\n", &Nb);
+    printf("%.12lf", Nb);
+
+
+
+
+    return 0;
+}
